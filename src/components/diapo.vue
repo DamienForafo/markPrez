b913a986a272baede6a65c1911b17386885bffaf
DamienForafo <damien.forafo@gmail.com>
2022-03-28 13:41:12 +0200

    xcdvfbf

--- src/components/diapo.vue
+++ src/components/diapo.vue
@@ -11,11 +11,17 @@ const props = defineProps({
 
 
 <template>
-  {{props.content}}
+  <div id="diapoContainer">{{props.content}}</div>
 </template>
 
 
 
 <style scoped>
-
+  #diapoContainer {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
 </style>
