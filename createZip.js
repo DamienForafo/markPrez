751368ada3a41631fc27fc6205dfcea93c5a1228
AlanAudran <65293707+AlanAudran@users.noreply.github.com>
2022-03-28 09:56:31 +0200

    zip

--- createZip.js
+++ createZip.js
@@ -1,41 +1,17 @@
-<<<<<<< HEAD
-const path = require('path');
-const fs = require ('fs/promises');
-=======
 const path = require('path')  ;
 const fs = require ('fs/promises') ;
->>>>>>> d9e39be2795ad3154539759c35adae9d8b35faae
 const archiver = require ('archiver');
 const replaceExt = require('replace-ext');
 
-<<<<<<< HEAD
-=======
-/*
-  const paths = ref({
-    md: '',
-    css: '',
-    config: '',
-    env: [],
-    assets: []
-  });
-*/
->>>>>>> d9e39be2795ad3154539759c35adae9d8b35faae
+
 
 
 async function zipDirectory(paths) {
 
-<<<<<<< HEAD
-async function zipDirectory(paths) {
-
-=======
->>>>>>> d9e39be2795ad3154539759c35adae9d8b35faae
   await fs.mkdir("./prez");
   const sourceDir = './prez' ;
   const outPath = './prez.zip' ;
 
-<<<<<<< HEAD
-  for (let file in paths)
-=======
   var products_file = path.basename(paths.md);
   fs.copy(
     paths.md,
@@ -82,18 +58,6 @@ async function zipDirectory(paths) {
   }
   
 
-/*
-  foreach(file in paths)
->>>>>>> d9e39be2795ad3154539759c35adae9d8b35faae
-  {
-    const products_file = path.basename(file);
-    await fs.copy(
-      file,
-      "./prez/" + products_file,
-      { overwrite: true }
-    );
-  }
-  */
     const archive = archiver('zip', { zlib: { level: 9 }});
     const stream = fs.createWriteStream(outPath);
   
@@ -112,10 +76,4 @@ async function zipDirectory(paths) {
       newPath = replaceExt(file, '.codeprez');
     });
   }
-<<<<<<< HEAD
-
-
-  module.exports = zipDirectory;
-=======
 module.exports = zipDirectory ;
->>>>>>> d9e39be2795ad3154539759c35adae9d8b35faae
